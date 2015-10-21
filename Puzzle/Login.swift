c84999c5e02110c6e4d2c470cfa3bd7a09d3b98d
Carlos Alejandro Reyna González <a01165824@DMDMASTER-4.local>
2015-10-21 14:04:32 -0500

    avance..

--- Puzzle/Login.swift
+++ Puzzle/Login.swift
@@ -11,25 +11,65 @@ import UIKit
 class Login: UIViewController {
 
     
-    //Outlets
-    @IBOutlet weak var santa: UILabel!
-    @IBOutlet weak var princess: UILabel!
-    @IBOutlet weak var allahuAkbar: UILabel!
-    @IBOutlet weak var bride: UILabel!
+    var loginChecker: [Int] = [Int](count: 3, repeatedValue: -1)
     
+    //Outlets
+    @IBOutlet weak var lbl_santa: UILabel!
+    
+    //Long press gesture
+    @IBAction func onLongPress(sender: AnyObject) {
+        if  (loginChecker[0] == -1 || loginChecker[0] == 1) &&
+            loginChecker[1] == -1 &&
+            loginChecker[2] == -1 {
+                
+                loginChecker[0] = 1
+                feedback(true)
+            
+        }else{
+            feedback(false)
+        }
 
-   
-    @IBAction func onLongPress(sender: UIPanGestureRecognizer) {
-        let touchPos = sender.locationInView(self.view)
-        let santaPos = santa.frame
-        println("touch \(touchPos)")
-        println("santa \(santaPos)")
         
     }
     
+    @IBAction func onLeftSwipe(sender: UISwipeGestureRecognizer) {
+        if loginChecker[0] == 1{
+            loginChecker[1] = 2
+        }else{
+            feedback(false)
+            loginChecker = [Int](count: 3, repeatedValue: -1)
+        }
+        
+    }
   
+    
+    @IBAction func onDownSwipe(sender: AnyObject) {
+        if loginChecker[1] == 2{
+            loginChecker[2] = 3
+            
+            performSegueWithIdentifier("toPuzzle", sender: self)
+           
+        }else{
+            feedback(false)
+            loginChecker = [Int](count: 3, repeatedValue: -1)
+        }
+        
+    }
+
+    
+ 
+    
+    func feedback(flag:Bool){
+        if flag{
+            self.view.backgroundColor = UIColor.greenColor()
+        }else{
+            self.view.backgroundColor = UIColor.redColor()
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        loginChecker = [Int](count: 3, repeatedValue: -1)
     }
 
     override func didReceiveMemoryWarning() {
--- Puzzle/Puzzle.swift
+++ Puzzle/Puzzle.swift
@@ -8,11 +8,94 @@
 
 import UIKit
 
-class Puzzle: UIViewController {
+class Puzzle: UIViewController, UICollectionViewDelegate,UICollectionViewDataSource,UIGestureRecognizerDelegate {
+    
+    //vars & cons
+    var pieces = ["puzzle_01.gif", "puzzle_02.gif", "puzzle_03.gif", "puzzle_04.gif", "puzzle_05.gif", "puzzle_06.gif", "puzzle_07.gif", "puzzle_08.gif", "puzzle_09.gif", "puzzle_10.gif", "puzzle_11.gif", "puzzle_12.gif", "puzzle_13.gif", "puzzle_14.gif", "puzzle_15.gif", "puzzle_16.gif"]
+    
+    var fromCell = UICollectionViewCell()
+    var toCell = UICollectionViewCell()
+    
+    
+    var timer : NSTimer = NSTimer()
+    let cellID = "piece"
+    
+    //Outlets
+    @IBOutlet weak var lbl_timeLeft: UILabel!
+    @IBOutlet weak var lbl_bestTime: UILabel!
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    
+    //Collection methods
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 16
+    }
+    
+    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+        //
+    }
+    
+    //fill collectionView
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell: UICollectionViewCell = collectionView.dequeueReusableCellWithReuseIdentifier(cellID, forIndexPath: indexPath) as! UICollectionViewCell
+        let piece:UIImageView = cell.viewWithTag(100) as! UIImageView
+        piece.image = UIImage(named: pieces[indexPath.row])
+        piece.image?.accessibilityIdentifier = pieces[indexPath.row]
+        return cell
+    }
+    
+    
+    //handeLongPress
+    func onLongPress(gestureRecognicer: UIPanGestureRecognizer){
+        var firstSelected = -1
+        var lastSelected = -1
+        
+        var point:CGPoint = gestureRecognicer.locationInView(self.collectionView)
+        var center = CGPointZero
+        
+        switch gestureRecognicer.state {
+        case .Began:
+            println("Began")
+            let indexPath = self.collectionView.indexPathForItemAtPoint(point)
+            if let index = indexPath{
+                self.fromCell = self.collectionView.cellForItemAtIndexPath(indexPath!)!
+                firstSelected = index.row
+                println("First selected = \(firstSelected)")
+                let piece:UIImageView = fromCell.viewWithTag(100) as! UIImageView
+                println(piece.image?.accessibilityIdentifier)
+            }
+        case .Changed:
+            let indexPath = self.collectionView.indexPathForItemAtPoint(point)
+            if let index = indexPath{
+
+                let piece:UIImageView = fromCell.viewWithTag(100) as! UIImageView
+                center = piece.center
+                self.fromCell.center.x = point.x
+                self.fromCell.center.y = point.y
+            }
+            
+        case .Ended:
+            println("Ended")
+            collectionView.reloadData()
+            
+        default:
+            println("Default??????")
+        }
+       
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let longPress = UIPanGestureRecognizer(target: self, action: "onLongPress:")
+        longPress.minimumNumberOfTouches = 1
+        longPress.delaysTouchesBegan = true
+        longPress.delegate = self
+        self.collectionView.addGestureRecognizer(longPress)
+
     }
     
     override func didReceiveMemoryWarning() {
